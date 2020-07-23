7e147474ec9602cc07111f3f802afa31054aec0b
Takanori Watanabe <takanori5064@gmail.com>
2020-07-23 20:09:31 +0900

    タスクを追加する機能の追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,13 +2,13 @@
   <div>
     {{ msg }}
     <form>
-      <button>タスクを追加</button>
+      <button type="button" v-on:click="addTodo()">タスクを追加</button>
       <button>終了したタスクを削除</button>
       <p>
         タスクを入力：
-        <input type="text" />
+        <input type="text" v-model="newTodo" />
       </p>
-      <p>タスク:</p>
+      <p>タスク: {{ newTodo }}</p>
     </form>
     <div class="task-list">
       <label class="task-list__item" v-for="todo in todos" :key="todo.text">
@@ -26,7 +26,7 @@ export default {
   props: {
     msg: String,
   },
-  data() {
+  data: () => {
     return {
       todos: [
         {
@@ -46,7 +46,22 @@ export default {
           done: true,
         },
       ],
+      newTodo: "",
     };
+  },
+  methods: {
+    // eslint-disable-next-line no-unused-vars
+    addTodo: function (event) {
+      let text = this.newTodo && this.newTodo.trim();
+      if (text.length === 0) {
+        return;
+      }
+      this.todos.push({
+        text: text,
+        done: false,
+      });
+      this.newTodo = "";
+    },
   },
 };
 </script>
