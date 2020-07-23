4d9d44ae4e3d2069be0c4455a52a8fe5117666c3
Takanori Watanabe <takanori5064@gmail.com>
2020-07-23 19:33:43 +0900

    View周りを追加

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
+    <!-- 
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    -->
+    <h1>Todo Management.</h1>
+    <hr />
+    <router-view />
   </div>
 </template>
 
